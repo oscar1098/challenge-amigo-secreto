770eba6ca66e79ceff4829e1af4352e99bed2cc4
oscar1098 <oscarardila1098@gmail.com>
2025-02-04 22:28:50 -0500

    Funcion actualizar lista de amigos

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 let amigos = []; 
 let amigo = document.getElementById("amigo"); 
+let listaAmigos = document.getElementById("listaAmigos"); 
 
 // Función que agrega nombres al array amigos
 function agregarAmigo(){
 
     if ( validarNombre() ){
-        amigos.push(amigo.value);
+        amigos.push(amigo.value); // Agregar amigo al array
+        crearListaAmigos(); // Crear la lista amigos en pantalla
     } 
 
     amigo.value = "";  // Limpiar el campo de texto
@@ -20,5 +22,21 @@ function validarNombre(){
         alert( "Por favor, inserte un nombre" );
     } else {
         return true;
+    }
+}
+
+// Crear lista de amigos en el HTML
+function crearListaAmigos(){
+
+    listaAmigos.innerHTML="";
+
+    for ( let i = 0; i <= amigos.length -1; i++  ){
+
+        let amigo = document.createElement( "li"); // Se crea la etiqueta li
+
+        amigo.textContent = amigos[i]; // Se le asigna a la etiqueta li
+
+        listaAmigos.appendChild(amigo); // Se añade la etiqueta li a la lista 
+        
     }
 }
